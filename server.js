c76f4ddc986e488eb8cf10ae12a830e8d89bf258
siddhantPaudel <siddhantpaudel777@gmail.com>
2023-07-06 08:45:10 +0545

    initialized

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.redirect(`${uuidv4()}`);
+})
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+
+server.listen(3000);

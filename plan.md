a0796374250efd7de5790ec9c517291a606d7557
siddhantPaudel <siddhantpaudel777@gmail.com>
2023-07-17 18:27:07 +0545

    added muteummute and playstop functionality

--- plan.md
+++ plan.md
@@ -4,8 +4,8 @@
 - Initiliaze the first view   DONE
 - create the room id  DONE
 - Add the ability to view our own video   DONE
-- Add abolity to allow others to stream their video
-- Add styling
-- Add the ability to create messages
-- Add mute button
-- Add stop video button 
+- Add abolity to allow others to stream their video DONE
+- Add styling  DONE
+- Add the ability to create messages DONE
+- Add mute button DONE
+- Add stop video button  DONE
--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ let myVideoStream;
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false,
+    audio: true,
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
@@ -40,6 +40,7 @@ navigator.mediaDevices.getUserMedia({
 
     socket.on('createMessage', message => {
         $('.messages').append(`<li class="message"><b>user</b><br/>${message}</li>`);
+        scrollToBottom()
     })
 })
 
@@ -66,3 +67,62 @@ const addVideoStream = (video, stream) => {
     videoGrid.append(video);
 }
 
+const scrollToBottom = () => {
+    var d = $('.main__chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    }
+    else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+    <i class ="fas fa-microphone"></i>
+    <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+const setUnmuteButton = () => {
+    const html = `
+    <i class =" unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    }
+    else {
+        setStopVideo();
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setPlayVideo = () => {
+    const html = `
+    <i class =" stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+    const html = `
+    <i class ="fas fa-video"></i>
+    <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
